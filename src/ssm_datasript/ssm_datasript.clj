859ac093da1bb0b6f82c1ea4dd17bd54e2d1193e
drew <drew.verlee@gmail.com>
2022-10-14 17:15:49 -0500

    init neil create

---
+++ src/ssm_datasript/ssm_datasript.clj
@@ -0,0 +1,12 @@
+(ns ssm-datasript.ssm-datasript
+  "FIXME: my new org.corfield.new/scratch project.")
+
+(defn exec
+  "Invoke me with clojure -X ssm-datasript.ssm-datasript/exec"
+  [opts]
+  (println "exec with" opts))
+
+(defn -main
+  "Invoke me with clojure -M -m ssm-datasript.ssm-datasript"
+  [& args]
+  (println "-main with" args))
